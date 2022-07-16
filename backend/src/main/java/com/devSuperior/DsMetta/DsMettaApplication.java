ddf0d6e461b4cd77ff1961a8c8aa24ecb28c55dd
Andrew-sobrera <andrewsobrera683@gmail.com>
2022-07-16 11:03:19 -0300

    primeiro commit

---
+++ backend/src/main/java/com/devSuperior/DsMetta/DsMettaApplication.java
@@ -0,0 +1,13 @@
+package com.devSuperior.DsMetta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsMettaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsMettaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devSuperior/DsMetta/DsMettaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devSuperior.DsMetta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsMettaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
